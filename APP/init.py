263b17af1d9b4b88a06778267172ac1c59ec9cd7
Srinath-N-Gudi <srinathngudigit11@gmail.com>
2025-03-22 16:28:32 +0530

    Working Basic Structure

--- APP/init.py
+++ APP/init.py
@@ -1,10 +1,8 @@
 from flask import Flask
-
+import os
 # Create the Flask app instance
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder=os.path.join(os.getcwd(), 'APP','templates'))
 # Load configuration from config.py
-app.config.from_object('config.Config')
+#app.config.from_object('config.Config')
 
 # Import routes to register them with the app
-from app import routes
--- APP/run.py
+++ APP/run.py
@@ -1,3 +1,22 @@
-from app import app
+from init import app
+import os
+from flask import render_template, request, redirect, url_for
+
+
+print(os.getcwd())
+# Homepage route
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Dashboard route
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+
+
+
 if __name__=='__main__':
        app.run(debug=True)
