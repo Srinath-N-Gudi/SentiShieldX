c697dce6aef5545df465685e4419713309e76bad
Srinath-N-Gudi <srinathngudigit11@gmail.com>
2025-03-24 23:37:57 +0530

    Working Website Structure

--- APP/init.py
+++ APP/init.py
@@ -1,7 +1,11 @@
 from flask import Flask
 import os
 # Create the Flask app instance
-app = Flask(__name__, template_folder=os.path.join(os.getcwd(), 'APP','templates'))
+
+templates_directory  = os.path.join(os.path.dirname(__file__), 'templates'  )
+static_directory = os.path.join(os.path.dirname(__file__), 'static')
+
+app = Flask(__name__, template_folder=templates_directory, static_folder=static_directory)
 # Load configuration from config.py
 #app.config.from_object('config.Config')
 
